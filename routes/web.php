a3fadef2a831b00af3043619f3dd1b2cfdf929da
ShiroExorcist <46170438+ShiroExorcist@users.noreply.github.com>
2024-03-07 10:59:59 +0300

    add admin, categoties

--- routes/web.php
+++ routes/web.php
@@ -13,6 +13,19 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
+Route::group(['namespace' => 'Main'], function () {
+    Route::get('/', 'IndexController');
+
 });
+
+Route::group(['namespace' => 'admin', 'prefix' => 'admin'], function () {
+    Route::group(['namespace' => 'Main'], function () {
+        Route::get('/', 'IndexController');
+    });
+    Route::group(['namespace' => 'Category', 'prefix' => 'categories'], function() {
+        Route::get('/','IndexController');
+    });
+});
+
+Auth::routes();
+
